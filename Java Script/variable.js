2f9e69925d64b9becdde4be8d4562f930e2315b3
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-06-11 14:53:08 +0530

    divya

--- Java Script/variable.js
+++ Java Script/variable.js
@@ -28,8 +28,19 @@
 
 
 
-let n=5;
-for(let i=1;i<=n;i++){
-    
-   
+// let n=5;
+// for(let i=1;i<=n;i++){
+//     for(let j=1;j<=n;j++){
+//         console.log(j)
+//     }
+//    console.log("")
+// }
+
+let n = 5; // height of triangle
+for (let i = 1; i <= n; i++) {
+  let row = "";
+  for (let j = 1; j <= i; j++) {
+    row += j + " ";
+  }
+  console.log(row);
 }
