e060be1adc79b853e0a4f46669f0cd2a9a8e44db
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-06-12 11:51:44 +0530

    divya

--- Java Script/function.js
+++ Java Script/function.js
@@ -1,9 +1,33 @@
-function add(x,y){
-    return x+y;
+// function add(x,y){
+//     return x+y;
+// }
+
+
+// console.log(add(10,6))
+// console.log(add("10",6))
+
+
+
+
+function greet(name){
+    console.log("Welcom " + name);
+    return "HELLLOOOOO"    // if return not defined than undefined will show in output with console value
 }
 
+function user(a){
+    return console.log(a("Divya"))
+}
 
-console.log(add(10,6))
+user(greet);
 
-console.log(add("10",6))
 
+
+// Function  Parameter and argument
+
+// function Expression
+
+
+// const hy=function greet(){
+//     console.log("Helooo")
+// }
+// console.log(hy)
