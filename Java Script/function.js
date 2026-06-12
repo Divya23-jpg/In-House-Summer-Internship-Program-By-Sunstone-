a6e84f5d7cbb5e6487dd31ffd22fee710ef6fdeb
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-06-12 12:04:07 +0530

    divya

--- Java Script/function.js
+++ Java Script/function.js
@@ -9,16 +9,16 @@
 
 
 
-function greet(name){
-    console.log("Welcom " + name);
-    return "HELLLOOOOO"    // if return not defined than undefined will show in output with console value
-}
+// function greet(name){
+//     console.log("Welcom " + name);
+//     return "HELLLOOOOO"    // if return not defined than undefined will show in output with console value
+// }
 
-function user(a){
-    return console.log(a("Divya"))
-}
+// function user(a){
+//     return console.log(a("Divya"))
+// }
 
-user(greet);
+// user(greet);
 
 
 
@@ -31,3 +31,19 @@ user(greet);
 //     console.log("Helooo")
 // }
 // console.log(hy)
+
+
+
+// ! High order function
+
+const numbers=[1,2,3,4,5];
+const t=function(num){
+    return num*3;
+}
+const triplet=numbers.map(function(num){
+    return num*3;
+})
+const trpletdemo=numbers.map(t);
+console.log(tripletdemo);
+
+console.log(triplet);
