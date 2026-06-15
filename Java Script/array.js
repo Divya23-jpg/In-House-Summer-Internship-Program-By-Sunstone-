664cae4dcceaa35c5cc3efd8136eaaaa7b7f2e6a
Divya23-jpg <divyakumawatofficial23@gmail.com>
2026-06-15 12:03:51 +0530

    divya

--- Java Script/array.js
+++ Java Script/array.js
@@ -1,9 +1,45 @@
 
 // !using array literal
 
-let fruits=["Apple","Banana","Orange"];
+let Fruits=["Apple","Banana","Orange"];
 
 // !using array Constructor
-let fruits=new Array("Apple","Banana","Orange");
+let fruits=new Array("Apple","Banana","Orange","Lichi","Mango");
+
+// console.log("Using Array literal :",Fruits)
+// console.log("Using Array Constructor:",fruits)
+
+// ! Access Element using Indexing
+console.log("Using Array Constructor:",fruits);
+console.log(fruits[0]);
+
+// ! length
+    // Size of array
+console.log(fruits.length);
+
+// ! push
+    // add element in  a last in an array
+    fruits.push("Watermelon");
+    console.log("after Push element :",fruits);
+
+// ! pop
+    // delete  element from  a last in an array
+     fruits.pop("Watermelon");
+    console.log("after pop element :",fruits);
+
+// ! unshift()
+    // Add element in a staring
+    fruits.unshift("Grapes");
+    console.log("After unshift",fruits);
+
+// ! shift()
+ // delete  element in a staring
+    fruits.shift();
+    console.log("After shift",fruits);
+
+// ! includes
+    // Check element is available or not
+    console.log(fruits.includes("Apple"));
+    console.log(fruits.includes("Grapes"));
 
 
